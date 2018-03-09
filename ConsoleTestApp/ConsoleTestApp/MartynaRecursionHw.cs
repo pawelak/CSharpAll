2419731c76d1faa19cd0908a874b6960aac03200
Paweł Kolak <kolakpk@gmail.com>
2018-03-09 09:50:42 +0100

    is working!

---
+++ ConsoleTestApp/ConsoleTestApp/MartynaRecursionHw.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace martyna2
+{
+    class MartynaRecursionHw
+    {
+
+        private List<int> list;
+
+        public MartynaRecursionHw(int end)
+        {
+            List = new List<int>();
+            for (int i = 0; i < end - 1; i++)
+            {
+                List.Add(0);
+            }
+        }
+
+        public List<int> List { get => list; set => list = value; }
+
+        public void Func(int end, int deep = 0)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (end <= deep + 1)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        foreach (var var in List)
+                        {
+                            Console.Write(var);
+                        }
+                        Console.WriteLine(j);
+                    }
+                }
+                else
+                {
+                    List[deep] = i;
+                    Func(end, deep + 1);
+                }
+
+            }
+        }
+
+    }
+}
---
+++ ConsoleTestApp/ConsoleTestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using martyna2;
+
+namespace ConsoleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var howMuchNumbers = 5;
+            var obj = new MartynaRecursionHw(howMuchNumbers);
+            obj.Func(howMuchNumbers);
+            Console.ReadKey();
+        }
+    }
+}
